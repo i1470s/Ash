a8c5985c38d9e46d341f03e1003683e8f3bf5a24
Brayden Viana <55937162+i1470s@users.noreply.github.com>
2021-03-08 14:02:11 -0800

    Create ash.py

---
+++ ash.py
@@ -0,0 +1,15 @@
+from PyQt5.QtWidgets import ( QMainWindow, QLabel, QDesktopWidget, QHBoxLayout,QFileDialog )
+from PyQt5.QtCore import Qt, QSettings, QSize, QPoint
+
+class App(QMainWindow):
+	is_save = False
+
+	def __init__(self):
+		super(App, self).__init__()
+		self.setWindowTitle("Ash")
+		
+		self.setGeometry(0, 0, 700, 600)
+		#self.setWindowFlag(Qt.FramelessWindowHint)
+		resolution = QDesktopWidget().screenGeometry()
+		self.move((resolution.width() / 2) - (self.frameSize().width() / 2),
+			(resolution.height() / 2) - (self.frameSize().height() / 2))
